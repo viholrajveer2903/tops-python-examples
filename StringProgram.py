2b5df4ce9e8dfcb9ca1777fbc6f56d798442b609
viholrajveer2903 <viholrajveer2903@gmail.com>
2022-12-19 12:27:03 +0530

    First Commit

---
+++ StringProgram.py
@@ -0,0 +1,20 @@
+s=input("Enter String : ")
+
+ch=0
+w=1
+sp=0
+n=0
+
+for i in s:
+    if i.isalpha():
+        ch=ch+1
+    elif i.isspace():
+        w=w+1
+        sp=sp+1
+    elif i.isnumeric():
+        n=n+1
+
+print("Total Character : ",ch)
+print("Total Word : ",w)
+print("Total Space : ",sp)
+print("Total Numeric : ",n)
---
+++ WhileDemo.py
@@ -0,0 +1,10 @@
+n=int(input("Enter N : "))
+sum=0
+
+while n>0:
+    sum=sum+n
+    n=n-1
+    
+print("Sum : ",sum)
+
+    
